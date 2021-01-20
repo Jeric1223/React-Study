ab84ac2219b2b0f370dc58e77b6c840a854a1c5b
JaeHyun <soehd0889@naver.com>
2021-01-20 23:07:27 +0900

    [2021-01-20] props 에 대해서 알아보았다

---
+++ 리액트를 다루는 기술/Hello React/hello-react/src/App.js
@@ -0,0 +1,8 @@
+import logo from "./logo.svg";
+import MyComponent from "./MyComponent";
+
+function App() {
+    return <MyComponent name='React' favoriteNumber={1} >리액트</MyComponent>;
+}
+
+export default App;
