8fb37b26c0726f3e8f9ffe198a891dad21bd1034
JaeHyun <soehd0889@naver.com>
2021-02-06 19:32:37 +0900

    [2021-02-06] styled-components 맛보기

--- styled-components/src/App.js
+++ styled-components/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import styled from "styled-components";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <Container>
+                <Button />
+                <Button danger />
+            </Container>
+        );
+    }
 }
+
+const Container = styled.div`
+    width: 100%;
+    height: 100vh;
+    background-color: gray;
+`;
+
+const Button = styled.button`
+    border-radius: 50px;
+    padding: 5px;
+    min-width: 120px;
+    color: white;
+    font-weight: 600;
+    background-color: ${props => (props.danger ? "#e74c3c" : "#2ecc71")}
+`;
 
 export default App;
