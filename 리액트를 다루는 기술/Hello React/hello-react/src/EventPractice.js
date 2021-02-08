4eed29d9ac45f8445234427dd05da23306e91394
JaeHyun <soehd0889@naver.com>
2021-02-08 22:42:25 +0900

    [2021-02-08] property initializer syntax 사용

--- 리액트를 다루는 기술/Hello React/hello-react/src/EventPractice.js
+++ 리액트를 다루는 기술/Hello React/hello-react/src/EventPractice.js
@@ -1,45 +1,44 @@
 import React, { Component, component } from "react";
 
 class EventPractice extends Component {
-    state = {
-        message: "",
-    };
+  state = {
+    message: "",
+  };
 
-    constructor(props) {
-        super(props);
-        this.handleChange = this.handleChange.bind(this);
-        this.handleClick = this.handleClick.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
 
-    handleChange(e) {
-        this.setState({
-            message: e.target.value,
-        });
-    }
+  handleChange = (e) => {
+    this.setState({
+      message: e.target.value,
+    });
+  };
 
-    handleClick() {
-        alert(this.state.message);
-        this.setState({
-            message: "",
-        });
-    }
+  handleClick = () => {
+    alert(this.state.message);
+    this.setState({
+      message: "",
+    });
+  };
 
-    handle;
-    render() {
-        return (
-            <div>
-                <h1>이벤트 연습</h1>
-                <input
-                    type="text"
-                    name="message"
-                    placeholder="아무거나 입력해 보세요"
-                    value={this.state.message}
-                    onChange={this.handleChange}
-                />
-                <button onClick={this.handleClick}>확인</button>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <h1>이벤트 연습</h1>
+        <input
+          type="text"
+          name="message"
+          placeholder="아무거나 입력해 보세요"
+          value={this.state.message}
+          onChange={this.handleChange}
+        />
+        <button onClick={this.handleClick}>확인</button>
+      </div>
+    );
+  }
 }
 
 export default EventPractice;
