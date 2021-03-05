3c03647b298d298f980f6a3a008dea8b4d26a004
JaeHyun <soehd0889@naver.com>
2021-03-05 17:10:04 +0900

    [2021-03-05] api test

--- 리액트를 다루는 기술/Hello React/hello-react/src/API TEST/ApiTest.js
+++ 리액트를 다루는 기술/Hello React/hello-react/src/API TEST/ApiTest.js
@@ -1,23 +1,31 @@
 import React, { useEffect, useState } from "react";
-import { request } from "../utils/axios";
+import axios from "axios";
 
 const Test = () => {
-  const [test, setTest] = useState("");
+  const [newsData, setNewsData] = useState([]);
 
-  useEffect(() => {
+  const getNewsData = async () => {
     try {
-      const data = request("get", `/report/52`, {}, {});
-      setTest(data);
-      console.log(data);
+      const data = await axios.get(
+        "http://newsapi.org/v2/everything?q=tesla&from=2021-02-04&sortBy=publishedAt&apiKey=e7c928fee9fe416db82160f07eab372c"
+      );
+      setNewsData(data.data.articles);
+      console.log(newsData);
     } catch (e) {
       console.log(e);
     }
-  }, []);
+  };
+
+  useEffect(() => {
+    getNewsData();
+  });
 
   return (
     <>
       <div>
-        {test.data.grade}
+        {newsData.map((title) => (
+          <h1 key={title.url}>{title.title}</h1>
+        ))}
       </div>
     </>
   );
