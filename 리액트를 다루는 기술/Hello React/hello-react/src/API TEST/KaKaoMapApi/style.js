36f02ec45d9d728a60af7b41fe1533483e398e83
JaeHyun <soehd0889@naver.com>
2021-03-09 08:20:00 +0900

    [2021-03-09] useCallback

---
+++ 리액트를 다루는 기술/Hello React/hello-react/src/API TEST/KaKaoMapApi/style.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const MainContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: black;
+  display: flex;
+  justify-content: center;
+  #map {
+    width: 500px;
+    height: 400px;
+    background-color: greenyellow;
+  }
+`;
