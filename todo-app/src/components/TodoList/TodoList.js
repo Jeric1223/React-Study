04555c528e2ca94652a74442cf6b2fcc9d9c1997
JaeHyun <soehd0889@naver.com>
2021-03-11 08:21:50 +0900

    [2021-03-11] 삭제 기능과  체크기능

--- todo-app/src/components/TodoList/TodoList.js
+++ todo-app/src/components/TodoList/TodoList.js
@@ -2,11 +2,16 @@ import React from 'react';
 import TodoListItem from '../TodoListItem/TodoListItem';
 import * as S from './style';
 
-const TodoList = ({ todos, onRemove }) => {
+const TodoList = ({ todos, onRemove, onToggle }) => {
   return (
     <S.TodoList>
       {todos.map((todo) => (
-        <TodoListItem todo={todo} key={todo.id} onRemove={onRemove} />
+        <TodoListItem
+          todo={todo}
+          key={todo.id}
+          onRemove={onRemove}
+          onToggle={onToggle}
+        />
       ))}
     </S.TodoList>
   );
