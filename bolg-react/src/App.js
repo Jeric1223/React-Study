4c03ad065981729e35ecb02fc437d1caef794c45
JaeHyun <soehd0889@naver.com>
2021-03-14 11:21:30 +0900

    [2021-03-14] 글로벌 스타일

--- bolg-react/src/App.js
+++ bolg-react/src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ShurikenGame from "./components/ShurikenGame/ShurikenGame";
+import { GlobalStyle } from "./GlobalStyle";
 
 const App = () => {
   return (
     <>
+      <GlobalStyle />
       <ShurikenGame />
     </>
   );
