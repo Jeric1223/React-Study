f0f039ede68af49894db65ef28b2fbc2cf503eef
JaeHyun <soehd0889@naver.com>
2021-03-14 11:22:06 +0900

    [2021-03-14] 전체적인 디자인, 로컬스토리지 사용, 돈 레벨기능 추가

--- bolg-react/src/components/ShurikenGame/ShurikenGame.js
+++ bolg-react/src/components/ShurikenGame/ShurikenGame.js
@@ -1,13 +1,21 @@
 import { findAllByDisplayValue } from "@testing-library/dom";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import * as S from "./style";
 
 const ShurikenGame = () => {
   const [animation, setAnimation] = useState(true);
+  const [money, setMoney] = useState(
+    () => JSON.parse(window.localStorage.getItem("money")) || 0
+  );
+  const [shurikenLevel, setShurikenLevel] = useState(
+    () => JSON.parse(window.localStorage.getItem("shurikenLevel")) || 0
+  );
 
   const onClick = (e) => {
     if (animation === true) {
       setAnimation(false);
+      setMoney(money + 1);
+      setShurikenLevel(shurikenLevel + 1);
       const buttonInterval = setInterval(() => {
         setAnimation(true);
       }, 2000);
@@ -17,18 +25,34 @@ const ShurikenGame = () => {
     }
   };
 
+  useEffect(() => {
+    window.localStorage.setItem("money", JSON.stringify(money));
+    window.localStorage.setItem("shurikenLevel", JSON.stringify(shurikenLevel));
+  }, [money, shurikenLevel]);
+
   return (
     <S.MainWrapper>
+      <h1>ShurikenGame</h1>
+      <div className="levelAndMoneyBox">
+        <p>Shuriken Level : {shurikenLevel}</p>
+        <p>money : {money}$</p>
+      </div>
       <div
-        className="asas"
+        className="shuriken"
         style={animation ? { animationName: "test" } : {}}
       ></div>
-      <button
-        onClick={onClick}
-        style={animation === false ? { background: "gray" } : {}}
-      >
-        {animation === true ? "던지기" : "되돌아 오는중"}
-      </button>
+      <div className="buttonContainer">
+        <button
+          onClick={onClick}
+          style={
+            animation === false
+              ? { background: "#161C40", color: "#0FF2B2" }
+              : { background: "#0FF2B2", color: "#161C40" }
+          }
+        >
+          {animation === true ? "Throw" : "Returning..."}
+        </button>
+      </div>
     </S.MainWrapper>
   );
 };
--- bolg-react/src/components/ShurikenGame/style.js
+++ bolg-react/src/components/ShurikenGame/style.js
@@ -7,7 +7,7 @@ export const test = keyframes`
     margin-left: 0px;
   }
   50% {
-    margin-left: 2000px;
+    margin-left: 1600px;
   }
   100% {
     margin-left: 0px;
@@ -21,23 +21,49 @@ export const MainWrapper = styled.div`
   height: 100%;
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  background-color: #5E5E5E;
+  background-color: #107369;
+  overflow: hidden;
+  h1 {
+    font-size: 75px;
+    font-family: "AppleSDGothicNeoEB00";
+    margin: 55px 0 0 73px;
+    color: #0FF2B2;
+  }
+  .levelAndMoneyBox{
+    width: 90%;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    margin-right: 75px;
+    margin-bottom: 42px;
+    font-size: 44px;
+    color: #0FF2B2;
+    font-family: AppleSDGothicNeoEB00;
+  }
+  .buttonContainer{
+    width: 100%;
+    display:flex;
+    justify-content: center;
+  }
   button {
-    width: 200px;
-    height: 50px;
-    border-radius: 20px;
+    width: 500px;
+    height: 75px;
+    border-radius: 40px;
+    border: none;
+    font-size: 24px;
+    font-family: "AppleSDGothicNeoEB00";
     &:focus {
       outline: none;
     }
   }
-  .asas {
+  .shuriken {
     width: 30%;
     height: 30%;
     background-image: url(${Shuriken2Level});
     background-repeat: no-repeat;
     background-size: 38%;
     animation: ${test} 2s;
-    margin-bottom: 50px;
+    margin-bottom: 100px;
+    margin-left: 73px;
   }
 `;
