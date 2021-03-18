f42bc8fc23bec91d60ca063443793bf14b98ed7c
JaeHyun <soehd0889@naver.com>
2021-03-18 15:51:37 +0900

    [2021-03-18] 레벨업 show hide 기능 구현

--- bolg-react/src/components/ShurikenGame/ShurikenGame.js
+++ bolg-react/src/components/ShurikenGame/ShurikenGame.js
@@ -3,6 +3,7 @@ import * as S from "./style";
 
 const ShurikenGame = () => {
   const [animation, setAnimation] = useState(true);
+  const [buttonToggle, setButtonToggle] = useState(true);
   const [money, setMoney] = useState(
     () => JSON.parse(window.localStorage.getItem("money")) || 0
   );
@@ -13,7 +14,7 @@ const ShurikenGame = () => {
     () => JSON.parse(window.localStorage.getItem("shurikenSpeed")) || 4
   );
 
-  const onClick = (e) => {
+  const onClick = () => {
     if (animation === true) {
       setAnimation(false);
       setMoney(money + shurikenLevel * 2);
@@ -33,6 +34,11 @@ const ShurikenGame = () => {
     console.log("도착함");
   };
 
+  const ButtonToggle = () => {
+    buttonToggle === false ? setButtonToggle(true) : setButtonToggle(false);
+    console.log("버튼 토글성공");
+  };
+
   useEffect(() => {
     window.localStorage.setItem("money", JSON.stringify(money));
     window.localStorage.setItem("shurikenLevel", JSON.stringify(shurikenLevel));
@@ -42,10 +48,10 @@ const ShurikenGame = () => {
   return (
     <S.MainWrapper>
       <h1>ShurikenGame</h1>
-      <S.levelAndMoneyBox>
+      <S.LevelAndMoneyBox>
         <p>Shuriken Level : {shurikenLevel}</p>
         <p>money : {money}$</p>
-      </S.levelAndMoneyBox>
+      </S.LevelAndMoneyBox>
       <S.Shuriken
         style={animation ? { animationName: "test" } : {}}
         shuriken
@@ -64,11 +70,12 @@ const ShurikenGame = () => {
         </button>
       </S.ButtonContainer>
       <S.UpgradeWrapper>
-        <S.UpgradeContainer>
-          <div>Upgrade</div>
+        <S.UpgradeContainer showBox={buttonToggle}>
+          <div onClick={ButtonToggle}>Upgrade</div>
           <p>
             표창을 업그레이드 합니다. 표창을 업그레이드 하면 돈이{" "}
-            {shurikenLevel * 2 + 2}$로 올라갑니다.
+            {shurikenLevel * 2 + 2}$로 올라갑니다. 스피드도{" "}
+            {shurikenSpeed - 0.25}상승합니다.
           </p>
           <h2>비용 : {shurikenLevel * 20}$</h2>
           <button
