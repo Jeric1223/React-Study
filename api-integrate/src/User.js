dfe63f5e830966349a77324e0d79d03688b088d8
JaeHyun <soehd0889@naver.com>
2021-04-11 22:35:53 +0900

    [2021-04-11] .

--- api-integrate/src/User.js
+++ api-integrate/src/User.js
@@ -21,6 +21,7 @@ function Users() {
   if (loading) return <div>로딩중..</div>;
   if (error) return <div>에러가 발생했습니다</div>;
   if (!users) return <button onClick={refetch}>불러오기</button>;
+  
   return (
     <>
       <ul>
