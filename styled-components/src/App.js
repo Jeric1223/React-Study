05ed8c981985ff2eca66174075a9bfb11a17a74f
JaeHyun <soehd0889@naver.com>
2021-04-13 23:26:46 +0900

    .

--- styled-components/src/App.js
+++ styled-components/src/App.js
@@ -2,29 +2,29 @@ import React, { Component } from "react";
 import styled from "styled-components";
 
 class App extends Component {
-    render() {
-        return (
-            <Container>
-                <Button />
-                <Button danger />
-            </Container>
-        );
-    }
+  render() {
+    return (
+      <Container>
+        <Button />
+        <Button danger />
+      </Container>
+    );
+  }
 }
 
 const Container = styled.div`
-    width: 100%;
-    height: 100vh;
-    background-color: gray;
+  width: 100%;
+  height: 100vh;
+  background-color: gray;
 `;
 
 const Button = styled.button`
-    border-radius: 50px;
-    padding: 5px;
-    min-width: 120px;
-    color: white;
-    font-weight: 600;
-    background-color: ${props => (props.danger ? "#e74c3c" : "#2ecc71")}
+  border-radius: 50px;
+  padding: 5px;
+  min-width: 120px;
+  color: white;
+  font-weight: 600;
+  background-color: ${(props) => (props.danger ? "#e74c3c" : "#2ecc71")};
 `;
 
 export default App;
